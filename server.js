fbdd96e85d5e32701f20f1f1f677c713f8606f38
oourdiaa <oourdia@yahoo.com>
2019-03-11 16:08:50 +0100

    chat bot

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-app.get('/', function (req, res) {
+app.get('/hello', function (req, res) {
   res.send('Hello World! Ourdia')
 })
 
+app.post('/chat', function (req, res) {
+	 const name = req.body.msg
+	 if(name=="ville"){
+	 	res.send("paris");
+	 }
+	 if(name=="météo"){
+	 		res.send("Il fait beau");
+	 }
+	 
+  
+})
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log(' listening on port 3000!')
 })

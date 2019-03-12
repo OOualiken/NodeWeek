30190ecde770268acaf0459b1a92f3fe53c65655
oourdiaa <oourdia@yahoo.com>
2019-03-12 12:24:01 +0100

    exercice 1.3 version

--- server.js
+++ server.js
@@ -1,26 +1,49 @@
-const express = require('express')
-const app = express()
-app.use(express.json())
+var express = require('express');
+const fs = require('fs');
 
-app.get('/hello', function (req, res) {
-  res.send('Hello World! Ourdia')
+var app = express();
+var port = process.env.PORT || 3000;
+app.use(express.json());
+
+console.log('Hello Word');
+
+app.listen(port, () => {
+  console.log("Listening on port 3000...");
 })
 
-app.get('/', function (req, res) {
-res.sendfile('formulaire.html');  
+app.get("/hello", (req, res) => {
+  res.send("Hello World\n");
 })
 
-app.post('/chat', function (req, res) {
-	 const name = req.body.msg
-	 if(name=="demain"){
-	 	res.send("paris");
-	 }
-	
-	 
-  
-})
-
-
-app.listen(process.env.PORT || 3000, function () {
-  console.log(' listening on port 3000!')
-})
+app.post('/chat', (req, res) => {
+  switch(req.body.msg) {
+    case "ville":
+      res.send("Nous sommes à Paris\n");
+      break;
+    case "météo":
+      res.send("Il fait beau\n");
+      break;
+    case "demain":
+      let rawdata = fs.readFileSync('response.json');
+      let json = JSON.parse(rawdata);
+      console.log(json.day);
+      if(json.day == null) {
+        res.send("Je ne connais pas demain…\n");
+      }
+      else {
+        res.send(json.day);
+      }
+      break;
+    case "demain = Mercredi":
+      let day = {
+          day: 'Mercredi',
+      };
+      let data = JSON.stringify(day);
+      fs.writeFileSync('response.json', data);
+      res.send("Mercredi");
+      break;
+    default:
+      res.send("Réponse par défaut\n");
+      break;
+  }
+});
